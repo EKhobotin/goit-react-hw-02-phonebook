e0fde68addb00bf05d8e8e07d5f5228c8e6bb1db
EKhobotin <evgen.29.3.85@gmail.com>
2023-12-15 23:25:13 +0200

    format pret

--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -1,15 +1,15 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
 export const List = styled.ul`
-list-style: none;
-`
+  list-style: none;
+`;
 export const ListItem = styled.li`
-margin: 0 auto;
-display:flex;
-justify-content: center; 
-`
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+`;
 export const Span = styled.span`
-margin: 0 10px;
-font-style: italic;
-font-weight: 500;
-`
+  margin: 0 10px;
+  font-style: italic;
+  font-weight: 500;
+`;
